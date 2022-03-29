8aa66e058a143ff5a02b1e42575bb39509b1e370
Elmer Astonitas <elmerastonitas@gmail.com>
2022-03-28 23:16:23 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'Controladores/plantilla.controlador.php';
+require_once "controladores/ruta.controlador.php";
+
+$plantilla = new ControladorPlantilla();
+$plantilla -> ctrPlantilla();
